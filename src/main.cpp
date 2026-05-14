dc0860b6f27fc5471c1d9a4a26cd6ed27fd38786
HurayrahB <huray2007@gmail.com>
2026-05-13 20:22:01 -0400

    Add cornell_smoke scene with constant medium
    Introduce a new cornell_smoke() scene that builds a Cornell box with two boxes wrapped in constant_medium to simulate smoke. Add #include "constant_medium.h" and register the scene in main by updating the switch from 7 to 8 and adding case 8. The new scene sets up materials, a light quad, two transformed boxes, camera parameters (aspect ratio, image width, samples, depth, vfov, lookfrom/lookat), and renders the world.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "material.h"
 #include "sphere.h"
 #include "quad.h"
+#include "constant_medium.h"
 
 void bouncing_spheres();
 void checkered_spheres();
@@ -15,6 +16,7 @@ void perlin_spheres();
 void quads();
 void simple_light();
 void cornell_box();
+void cornell_smoke();
 
 int main();
 
@@ -264,8 +266,52 @@ void cornell_box() {
     cam.render(world);
 }
 
+void cornell_smoke() {
+    hittable_list world;
+
+    auto red   = make_shared<lambertian>(colour(.65, .05, .05));
+    auto white = make_shared<lambertian>(colour(.73, .73, .73));
+    auto green = make_shared<lambertian>(colour(.12, .45, .15));
+    auto light = make_shared<diffuse_light>(colour(7, 7, 7));
+
+    world.add(make_shared<quad>(point3(555,0,0), vec3(0,555,0), vec3(0,0,555), green));
+    world.add(make_shared<quad>(point3(0,0,0), vec3(0,555,0), vec3(0,0,555), red));
+    world.add(make_shared<quad>(point3(113,554,127), vec3(330,0,0), vec3(0,0,305), light));
+    world.add(make_shared<quad>(point3(0,555,0), vec3(555,0,0), vec3(0,0,555), white));
+    world.add(make_shared<quad>(point3(0,0,0), vec3(555,0,0), vec3(0,0,555), white));
+    world.add(make_shared<quad>(point3(0,0,555), vec3(555,0,0), vec3(0,555,0), white));
+
+    shared_ptr<hittable> box1 = box(point3(0,0,0), point3(165,330,165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+
+    shared_ptr<hittable> box2 = box(point3(0,0,0), point3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+
+    world.add(make_shared<constant_medium>(box1, 0.01, colour(0,0,0)));
+    world.add(make_shared<constant_medium>(box2, 0.01, colour(1,1,1)));
+
+    camera cam;
+
+    cam.aspect_ratio      = 1.0;
+    cam.image_width       = 600;
+    cam.samples_per_pixel = 200;
+    cam.max_depth         = 50;
+    cam.background        = colour(0,0,0);
+
+    cam.vfov     = 40;
+    cam.lookfrom = point3(278, 278, -800);
+    cam.lookat   = point3(278, 278, 0);
+    cam.v_up      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
 int main() {
-    switch (7) {
+    switch (8) {
         case 1:
             bouncing_spheres();
             break;
@@ -286,6 +332,9 @@ int main() {
             break;
         case 7:
             cornell_box();
+            break;
+        case 8:
+            cornell_smoke();
             break;
     }
 }
