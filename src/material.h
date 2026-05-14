edab56e556acb1e790fbdff5d10a9817fc4af59c
HurayrahB <huray2007@gmail.com>
2026-05-13 20:18:13 -0400

    Add constant_medium volume and isotropic material
    Introduce a constant_medium hittable for participating media (volumes) that wraps a boundary hittable and samples scattering events based on density and a phase function. Provides constructors accepting a texture or an albedo; hit() computes entry/exit intersections, distance inside the boundary, and a sampled scattering point. Also add isotropic material to material.h to serve as the phase function (uses a texture or colour and scatters uniformly). Bounding box lookup delegates to the boundary.

--- src/material.h
+++ src/material.h
@@ -9,6 +9,7 @@ class lambertian;
 class metal;
 class dielectric;
 class diffuse_light;
+class isotropic;
 
 class material {
     public:
@@ -112,6 +113,22 @@ class diffuse_light : public material {
         colour emitted(double u, double v, const point3& p) const override {
             return tex->value(u, v, p);
         }
+
+    private:
+        shared_ptr<texture> tex;
+};
+
+class isotropic : public material {
+    public:
+    isotropic(const colour& albedo) : tex(make_shared<solid_colour>(albedo)) {}
+    isotropic(shared_ptr<texture> tex) : tex(tex) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, colour& attenuation, ray& scattered) 
+     const override {
+        scattered = ray(rec.p, random_unit_vector(), r_in.time());
+        attenuation = tex->value(rec.u, rec.v, rec.p);
+        return true;
+     }
 
     private:
         shared_ptr<texture> tex;
